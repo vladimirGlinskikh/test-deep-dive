7dd2a1e8ee991a8b0f1903489695d22952b6f60f
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-11-16 23:43:06 +0300

    Started project

---
+++ src/main/java/kz/zhelezyaka/testdeepdive/TestDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.testdeepdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeepDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeepDiveApplication.class, args);
+	}
+
+}
